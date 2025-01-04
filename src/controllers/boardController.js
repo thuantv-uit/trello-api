4225dd4d947e342e99c1a9bef970efa3aaef039c
tvt04102004@gmail.com <tvt04102004@gmail.com>
2025-01-04 20:40:14 +0700

    Thuan commit: Error Handling, xử lý lỗi tập trung phía Back-end

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,14 +1,19 @@
 import { StatusCodes } from 'http-status-codes'
+// import ApiError from '~/utils/ApiError'
 
 const CreateNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
+    // console.log('req.body: ', req.body)
+    // console.log('req.query: ', req.query)
+    // console.log('req.params: ', req.params)
+    // console.log('req.files: ', req.files)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
+
+    // Điều hướng dữ liệu sang tầng Service
+    // Có kết quả thì trả về phía Client
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board' })
-  } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
-  }
+  } catch (error) { next(error) }
 }
 
 export const boardController = {
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
@@ -11,6 +12,9 @@ const START_SERVER = () => {
   app.use(express.json())
   // Use APIs V1
   app.use('/v1', APIs_V1)
+
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware)
 
   app.get('/', (req, res) => {
     res.end('<h1>Hello World!</h1><hr>')
